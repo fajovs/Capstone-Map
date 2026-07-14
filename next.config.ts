428fb2f7828f6c8eddaede47062de6760acadf8c
theamaire <dadortheamaire17@gmail.com>
2026-07-14 18:21:26 +0800

    with android

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: {
     domains: ["rvocpntyjcskssrbcoic.supabase.co"],
+    unoptimized: true,
   },
+ 
 };
 
 export default nextConfig;
